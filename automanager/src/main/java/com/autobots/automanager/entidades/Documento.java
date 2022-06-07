431f5f92bb3e06f5fd6a318301233d1ad8a0630c
thhenrique <henriquethiago298@gmail.com>
2022-06-06 22:16:06 -0300

    starting atv 3 - adding new entities

--- automanager/src/main/java/com/autobots/automanager/entidades/Documento.java
+++ automanager/src/main/java/com/autobots/automanager/entidades/Documento.java
@@ -1,29 +1,27 @@
 package com.autobots.automanager.entidades;
 
+import java.util.Date;
+
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.springframework.hateoas.RepresentationModel;
+import com.autobots.automanager.enumeracoes.TipoDocumento;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @Entity
-@Embeddable
-public class Telefone extends RepresentationModel<Telefone> {
-	@Id()
+public class Documento {
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column
-	private String ddd;
-	@Column
+	@Column(nullable = false)
+	private TipoDocumento tipo;
+	@Column(nullable = false)
+	private Date dataEmissao;
+	@Column(unique = true, nullable = false)
 	private String numero;
-	@Column
-	private Long clienteId;
 }
